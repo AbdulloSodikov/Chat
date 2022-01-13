d3a7c21451c2b337bb0677cc882e4c3f73adbc27
Sodikov Abdullo <sodikov.ak@gmail.com>
2022-01-13 13:17:25 +0500

    Person Adapter

---
+++ app/src/main/java/com/example/chat/ui/start_fragment/PersonAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.chat.ui.start_fragment
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.chat.R
+import com.example.chat.model.Person
+
+class PersonAdapter : RecyclerView.Adapter<PersonAdapter.PersonHolder>() {
+    private var mPersonList = emptyList<Person>()
+
+
+    class PersonHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val urlAvatarPerson: ImageView = view.findViewById(R.id.person_image)
+        val fullName: TextView = view.findViewById(R.id.tv_full_name)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PersonHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.person_item, parent, false)
+        return PersonHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PersonHolder, position: Int) {
+        val person = mPersonList[position]
+        holder.fullName.text = person.personName
+
+
+            Glide.with(holder.urlAvatarPerson.context)
+                .load(mPersonList[position].urlPersonAvatar)
+                .into(holder.urlAvatarPerson)
+
+    }
+
+    override fun getItemCount(): Int = mPersonList.size
+
+    fun setList(list: List<Person>) {
+        mPersonList = list
+        notifyDataSetChanged()
+
+    }
+
+}
